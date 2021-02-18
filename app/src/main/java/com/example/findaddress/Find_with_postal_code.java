d221f26449f76f1dda2ded8a1cb63f6daf5462f9
kaua vitorio <kauavitorioof@gmail.com>
2021-02-18 17:00:18 -0300

    Set string and translation

--- app/src/main/java/com/example/findaddress/Find_with_postal_code.java
+++ app/src/main/java/com/example/findaddress/Find_with_postal_code.java
@@ -13,10 +13,12 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.findaddress.Classes.MaskEditUtil;
 import com.example.findaddress.Threads.AsyncCEP;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Find_with_postal_code extends AppCompatActivity {
     TextView txt_aguardando_cep, txt_resultOf;
@@ -30,23 +32,7 @@ public class Find_with_postal_code extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_with_postal_code);
-        txt_aguardando_cep = findViewById(R.id.txt_aguardando_cep);
-        edittext_CEP = findViewById(R.id.edittext_CEP);
-        cardBtnSearch = findViewById(R.id.cardBtnSearch);
-        cardBtnGoBack = findViewById(R.id.cardBtnGoBack);
-        cardBtnNewSearch = findViewById(R.id.cardBtnNewSearch);
-        baseSearch = findViewById(R.id.baseSearch);
-        baseResult = findViewById(R.id.baseResult);
-        txt_address_cep = findViewById(R.id.txt_address_cep);
-        txt_complement_cep = findViewById(R.id.txt_complement_cep);
-        txt_district_cep = findViewById(R.id.txt_district_cep);
-        txt_localidade_cep = findViewById(R.id.txt_localidade_cep);
-        txt_state_cep = findViewById(R.id.txt_state_cep);
-        txt_ibge_cep = findViewById(R.id.txt_ibge_cep);
-        txt_gia_cep = findViewById(R.id.txt_gia_cep);
-        txt_ddd_cep = findViewById(R.id.txt_ddd_cep);
-        txt_siafi_cep = findViewById(R.id.txt_siafi_cep);
-        txt_resultOf = findViewById(R.id.txt_resultOf);
+        Ids();
         edittext_CEP.addTextChangedListener(MaskEditUtil.mask(edittext_CEP, MaskEditUtil.FORMAT_CEP));
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
@@ -77,9 +63,31 @@ public class Find_with_postal_code extends AppCompatActivity {
         });
 
         cardBtnSearch.setOnClickListener(v -> {
-            if (edittext_CEP.getText().length() == 9){
+            String zipcode = edittext_CEP.getText().toString();
+            Pattern pattern_zipcode = Pattern.compile("(^\\d{5}-\\d{3}|^\\d{2}.\\d{3}-\\d{3}|\\d{8})");
+            Matcher matcher = pattern_zipcode.matcher(zipcode);
+            if(zipcode.equals("")){
+                // Do your action here if the field is empty.
+                edittext_CEP.setError("O campo não pode estar vazio");
+                edittext_CEP.requestFocus();
+                imm.showSoftInput(edittext_CEP, InputMethodManager.SHOW_IMPLICIT);
+            }
+            if (!matcher.matches()) {
+                // Make your action here if the CEP is invalid.
+                edittext_CEP.setError("Informe um CEP válido");
+                edittext_CEP.requestFocus();
+            } else {
+                // If everything is OK perform the action here.
+                AsyncCEP asyncCEP = new AsyncCEP(Find_with_postal_code.this, txt_address_cep, txt_complement_cep, txt_district_cep , txt_localidade_cep, txt_state_cep, txt_ibge_cep, txt_gia_cep, txt_ddd_cep, txt_siafi_cep, zipcode);
+                asyncCEP.execute();
+                baseResult.setVisibility(View.VISIBLE);
+                baseSearch.setVisibility(View.GONE);
+                txt_resultOf.setText("Esses são os resultados de: "+ zipcode);
+                imm.hideSoftInputFromWindow(edittext_CEP.getWindowToken(), 0);
+            }
+         /*   if (edittext_CEP.getText().length() == 9){
                 String cep = edittext_CEP.getText().toString();
-                AsyncCEP asyncCEP = new AsyncCEP(txt_address_cep, txt_complement_cep, txt_district_cep , txt_localidade_cep, txt_state_cep, txt_ibge_cep, txt_gia_cep, txt_ddd_cep, txt_siafi_cep, cep);
+                AsyncCEP asyncCEP = new AsyncCEP(Find_with_postal_code.this, txt_address_cep, txt_complement_cep, txt_district_cep , txt_localidade_cep, txt_state_cep, txt_ibge_cep, txt_gia_cep, txt_ddd_cep, txt_siafi_cep, cep);
                 asyncCEP.execute();
                 baseResult.setVisibility(View.VISIBLE);
                 baseSearch.setVisibility(View.GONE);
@@ -89,7 +97,7 @@ public class Find_with_postal_code extends AppCompatActivity {
                 Toast.makeText(this, "CEP preenchido incorretamente!!", Toast.LENGTH_SHORT).show();
                 edittext_CEP.requestFocus();
                 imm.showSoftInput(edittext_CEP, InputMethodManager.SHOW_IMPLICIT);
-            }
+            }*/
         });
 
         cardBtnNewSearch.setOnClickListener(v -> LimparTudo());
@@ -111,14 +119,47 @@ public class Find_with_postal_code extends AppCompatActivity {
         txt_gia_cep.setText("GIA: ");
         txt_ddd_cep.setText("DDD: ");
         txt_siafi_cep.setText("SIAFI: ");
+        ShowTextResult();
         baseResult.setVisibility(View.GONE);
         baseSearch.setVisibility(View.VISIBLE);
         edittext_CEP.requestFocus();
         imm.showSoftInput(edittext_CEP, InputMethodManager.SHOW_IMPLICIT);
     }
 
+    private void ShowTextResult() {
+        txt_address_cep.setVisibility(View.VISIBLE);
+        txt_complement_cep.setVisibility(View.VISIBLE);
+        txt_district_cep.setVisibility(View.VISIBLE);
+        txt_localidade_cep.setVisibility(View.VISIBLE);
+        txt_state_cep.setVisibility(View.VISIBLE);
+        txt_ibge_cep.setVisibility(View.VISIBLE);
+        txt_gia_cep.setVisibility(View.VISIBLE);
+        txt_ddd_cep.setVisibility(View.VISIBLE);
+        txt_siafi_cep.setVisibility(View.VISIBLE);
+    }
+
     @Override
     public void onBackPressed() {
         finish();
+    }
+
+    private void Ids() {
+        txt_aguardando_cep = findViewById(R.id.txt_aguardando_cep);
+        edittext_CEP = findViewById(R.id.edittext_CEP);
+        cardBtnSearch = findViewById(R.id.cardBtnSearch);
+        cardBtnGoBack = findViewById(R.id.cardBtnGoBack);
+        cardBtnNewSearch = findViewById(R.id.cardBtnNewSearch);
+        baseSearch = findViewById(R.id.baseSearch);
+        baseResult = findViewById(R.id.baseResult);
+        txt_address_cep = findViewById(R.id.txt_address_cep);
+        txt_complement_cep = findViewById(R.id.txt_complement_cep);
+        txt_district_cep = findViewById(R.id.txt_district_cep);
+        txt_localidade_cep = findViewById(R.id.txt_localidade_cep);
+        txt_state_cep = findViewById(R.id.txt_state_cep);
+        txt_ibge_cep = findViewById(R.id.txt_ibge_cep);
+        txt_gia_cep = findViewById(R.id.txt_gia_cep);
+        txt_ddd_cep = findViewById(R.id.txt_ddd_cep);
+        txt_siafi_cep = findViewById(R.id.txt_siafi_cep);
+        txt_resultOf = findViewById(R.id.txt_resultOf);
     }
 }
